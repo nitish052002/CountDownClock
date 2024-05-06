48f5ea0adf39a30b163db0d5be4702ad714b3f26
Nitish <nitishsharma052002@gmail.com>
2024-05-07 02:24:14 +0530

    startcountdown stopcountdown and getuserdate functins been implemented

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,114 @@ import styles from "./App.module.css";
 import DateInput from "./components/DateInput";
 import GradientBox from "./components/GradientBox";
 
+interface DateTime {
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
+
 const App: React.FC = () => {
+  const [cancelIntervalId, setCancelIntervalId] = useState<number | null>(null);
+  const [cancelBtn, setCancelBtn] = useState<boolean>(false);
+  const [userDate, setUserDate] = useState<string | null>(null);
+  const [dateTime, setDateTime] = useState<DateTime>({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+  const [error, setError] = useState<string | null>(null);
+
+  function generateError(message: string): void {
+    setError(message);
+    setTimeout(() => {
+      setError(null);
+    }, 2000);
+  }
+
+  function clock(): void {
+    const START_DATE: Date = new Date();
+    const END_DATE: Date = new Date(userDate || 0);
+    const difference: number = END_DATE.getTime() - START_DATE.getTime();
+
+    let seconds: number = Math.floor(difference / 1000);
+    let minutes: number = Math.floor(seconds / 60);
+    let hours: number = Math.floor(minutes / 60);
+    const days: number = Math.floor(hours / 24);
+
+    seconds %= 60;
+    minutes %= 60;
+    hours %= 24;
+
+    if (days > 100) {
+      generateError("Selected Time is more than 100 days");
+      return;
+    }
+
+    setDateTime({
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    });
+  }
+
+  useEffect(() => {
+    if (cancelBtn) {
+      generateError("Countdown is running");
+    }
+
+    if (userDate) {
+      clock();
+    }
+  }, [userDate]);
+
+  function startCountDown(): void {
+    if (!userDate) {
+      generateError("Select date and time");
+      return;
+    }
+
+    if (dateTime.days > 100) {
+      generateError("Selected Time is more than 100 days");
+      return;
+    }
+
+    setCancelBtn(true);
+    const intervalId: number = setInterval(() => {
+      if (userDate) {
+        clock();
+      }
+    }, 1000);
+    setCancelIntervalId(intervalId);
+  }
+
+  function stopCountDown(): void {
+    if (cancelIntervalId) {
+      clearInterval(cancelIntervalId);
+    }
+
+    setDateTime({
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    });
+
+    setUserDate(null);
+    setCancelBtn(false);
+  }
+
+  function getUserDate(event: ChangeEvent<HTMLInputElement>): void {
+    if (userDate) {
+      generateError("Countdown is running");
+      return;
+    }
+    const DATE_BYUSER: string = event.target.value;
+    setUserDate(DATE_BYUSER);
+  }
+
   return (
     <main className="app">
       <div className={styles.title}>
@@ -11,18 +118,29 @@ const App: React.FC = () => {
       </div>
 
       <div className={styles.row}>
-        <DateInput clockCallBack={} />
+        <DateInput clockCallBack={getUserDate} />
       </div>
 
       <div className={styles.row}>
-        <button className={styles.startBtn}>Cancel</button>
-
-        <button className={styles.startBtn}>Start Now</button>
+        {cancelBtn ? (
+          <button className={styles.startBtn} onClick={stopCountDown}>
+            Cancel
+          </button>
+        ) : (
+          <button className={styles.startBtn} onClick={startCountDown}>
+            Start Now
+          </button>
+        )}
       </div>
 
       <div className={styles.row}>
-        <GradientBox label="Days" />
+        <GradientBox label="Days" number={dateTime.days} />
+        <GradientBox label="Hours" number={dateTime.hours} />
+        <GradientBox label="Minutes" number={dateTime.minutes} />
+        <GradientBox label="Seconds" number={dateTime.seconds} />
       </div>
+
+      <div className={styles.errContainer}>{error && <p>{error}</p>}</div>
     </main>
   );
 };
